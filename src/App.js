f89cdb6c187ee28394b5988f32e9eef973289150
sathaphorn.stp <sathaphorn.stp@gmail.com>
2019-12-12 17:01:49 +0700

    add logo and layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import { voteAngular, voteReact, voteVuejs } from './actions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.store = this.props.store;
+  }
+
+  handleVoteAngular = () => {
+    this.store.dispatch(voteAngular());
+  };
+
+  handleVoteReact = () => {
+    this.store.dispatch(voteReact());
+  };
+
+  handleVoteVuejs = () => {
+    this.store.dispatch(voteVuejs());
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="jumbotron" style={{textAlign: 'center'}}>
+          <img src="images/redux_logo.png" height="96" alt="logo"/>
+          <h2>What is your favorite front-end framework in 2019?</h2>
+          <h4>Click on the logos below to vote!</h4>
+          <br/>
+          <div className="row">
+            <div className="offset-3 col-2">
+              <img src="images/angular_logo.png" height="96" alt="Angular" onClick={this.handleVoteAngular}/>
+            </div>
+            <div className="col-2">
+              <img src="images/react_logo.png" height="96" alt="React" onClick={this.handleVoteReact}/>
+            </div>
+            <div className="col-2">
+              <img src="images/vuejs_logo.png" height="96" alt="Vuejs" onClick={this.handleVoteVuejs}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
 import App from './App';
+import myApp from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let store = createStore(myApp);
+
+function render() {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
